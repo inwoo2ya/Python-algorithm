0d59bef1b2db522f78905168656a2b1680f40864
inwoo2ya <86223023+inwoo2ya@users.noreply.github.com>
2022-04-28 21:10:25 +0900

    Update 2231 분해합.py

--- 2231 분해합.py
+++ 2231 분해합.py
@@ -1,12 +1,12 @@
-m = int(input())
-for i in range(1,m+1) :
-    num = sum(map(int,str(i))) # 계속 반복해서 m까지 올라가면서 각 자릿수 int로 바꿔서 더하기
-    print(num,"+",i)
-    nsum = i + num
-    if m == nsum :
+n = int(input())
+
+for i in range(n+1):
+    m = sum(map(int,str(i))) # if 231일경우 m= 2+3+1
+    msum = m+i #if 231일 경우 msum = 2+3+1  +  231
+
+    if n == msum :
         print(i)
         break
-    if m == i :
+    elif n == i :
         print(0)
         break
-    
