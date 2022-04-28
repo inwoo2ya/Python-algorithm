2baea25cd2d8cf1714d44cf3cefd47ebbcdc9ac9
inwoo2ya <86223023+inwoo2ya@users.noreply.github.com>
2022-04-28 21:03:09 +0900

    Update 2798 블랙잭.py

--- 2798 블랙잭.py
+++ 2798 블랙잭.py
@@ -5,9 +5,10 @@ result = 0
 for i in range(n):
     for j in range(i+1,n):
         for k in range(j+1,n):
-            if card[i] + card[j] + card[k] > m :
+            if card[i] + card[j] + card[k] >m :
                 continue
-            else :
-                result = max(result, card[i]+ card[j]+ card[k])
+            else:
+                result = max(result,card[i]+card[j]+card[k]) # result와 card값들을 더한 것중에 더 큰 것을 비교후 result값에 대입
+
 print(result)
-    
+        
