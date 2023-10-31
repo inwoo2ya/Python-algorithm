5f9da3184720e94a053898fdd7e74c42258f8366
inwoo Lee <86223023+inwoo2ya@users.noreply.github.com>
2023-11-01 02:35:41 +0900

    Add files via upload

--- 웅진씽크빅 코딩테스트 2번.py
+++ 웅진씽크빅 코딩테스트 2번.py
@@ -1,8 +1,8 @@
 s = '1 2 3 4 B B 42 B F F'
-page = []
-currentPage = []
-nextPage =[]
-total = []
+page = []  # 이동 페이지 경로
+currentPage = [] #현재 페이지 
+nextPage =[] # 다음 이동할 수 있는 페이지 
+total = [] #페이지가 렌더링된 수 
 for i in s.split():
     if i == 'B':
         if len(currentPage ) != 0 :
@@ -19,10 +19,11 @@ for i in s.split():
     else:
         currentPage.append(i)
         page.append(i)
-        nextPage = []
+        nextPage = [] #홈페이지 이동 시 다음 페이지 초기화
 
 e = list(set(page))
 for i in e:
     total.append(page.count(i))
 
-print(max(total))
+print(max(total)) #가장 많이 이동한(렌더링된) 페이지의 수
+
